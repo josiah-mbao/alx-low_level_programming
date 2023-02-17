f836922c96039976fbe3410521f62b7e847810bf
josiah <josiahmbaomc@gmail.com>
2023-02-17 15:06:44 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+/**
+ * main - Prints a text accordingto number
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+	char low;
+
+	for (low = 'z'; low <= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
