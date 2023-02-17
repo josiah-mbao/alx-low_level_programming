f5b57c7d0ae426e7c4c8856c723fee7009c71ec0
josiah <josiahmbaomc@gmail.com>
2023-02-17 14:53:05 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+/**
+ * main - Prints a text accordingto number
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
