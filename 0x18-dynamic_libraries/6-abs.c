f3f0d93ce6b603a3166d569a87e52d798a4cdff6
josiah-mbao <josiahmbaomc@gmail.com>
2023-06-19 12:39:59 -0700

    0x18 tasks

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the inegert to check
+ * Return: the abs value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
